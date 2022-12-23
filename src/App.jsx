4f7a985496699f1ea323ed9915163b9ac0405d89
Goik <sbabinski@outlook.com>
2022-12-23 15:15:52 -0300

    Create component files
    Create component files and names.
    Tinker about component structure.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  render() {
+    return <div>this</div>;
+  }
+}
+
+export function Header(){
+  return <header>CV GENERATOR</header>
+}
+
+export function Footer(){
+  return <footer>Created by<a href="https://github.com/Goik28"> Goik28 </a>for the CV-Application project on The Odin Project course</footer>
+}
